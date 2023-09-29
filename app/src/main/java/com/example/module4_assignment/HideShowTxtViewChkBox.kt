9d99f67145a3e9205295e0b8208705e633309ef8
harshida7 <141333123+harshida7@users.noreply.github.com>
2023-09-29 11:47:27 +0530

    Module 4 Assignement

---
+++ app/src/main/java/com/example/module4_assignment/HideShowTxtViewChkBox.kt
@@ -0,0 +1,31 @@
+package com.example.module4_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+
+class HideShowTxtViewChkBox : AppCompatActivity() {
+
+    lateinit var txtHS:TextView
+    lateinit var chkHS:CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hide_show_txt_view_chk_box)
+
+        txtHS = findViewById(R.id.mahadevTxt)
+        chkHS = findViewById(R.id.ChkHS)
+
+
+        chkHS.setOnClickListener {
+
+            if (chkHS.isChecked) {
+                txtHS.visibility = View.INVISIBLE
+            } else {
+                txtHS.visibility = View.VISIBLE
+            }
+        }
+    }
+}
